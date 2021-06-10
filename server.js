c3d930be878f112c0897a25db8c5d0313ba5295a
hustle-coder <ajd1999@live.com>
2021-06-10 20:28:58 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,75 @@
+const path = require("path");
+const mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: process.env.db_host,
+  user: process.env.db_user,
+  password: process.env.db_password,
+  database: process.env.db_name
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+
+// Require the fastify framework and instantiate it
+const fastify = require("fastify")({
+  // set this to true for detailed logging:
+  logger: true
+});
+
+// Setup our static files
+fastify.register(require("fastify-static"), {
+  root: path.join(__dirname, "public"),
+  prefix: "/" // optional: default '/'
+});
+
+// fastify-formbody lets us parse incoming forms
+fastify.register(require("fastify-formbody"));
+
+// point-of-view is a templating manager for fastify
+fastify.register(require("point-of-view"), {
+  engine: {
+    handlebars: require("handlebars")
+  }
+});
+
+// Our main GET home page route, pulls from src/pages/index.hbs
+fastify.get("/", function(request, reply) {
+  // params is an object we'll pass to our handlebars template
+  let params = {
+    greeting: "Hello Node!"
+  };
+  // request.query.paramName <-- a querystring example
+  reply.view("/src/pages/index.hbs", params);
+});
+
+// A POST route to handle form submissions
+fastify.post("/", function(request, reply) {
+  let params = {
+    greeting: "Hello Form!"
+  };
+  // request.body.paramName <-- a form post example
+  reply.view("/src/pages/index.hbs", params);
+});
+
+fastify.get("/api/v1/modules", function(request, reply){
+  const sql = 'select * from temp'
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    reply.send({data: result, type: typeof result});
+    console.log("Result: " + result);
+  });
+})
+
+// Run the server and report out to the logs
+fastify.listen(process.env.PORT, function(err, address) {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  console.log(`Your app is listening on ${address}`);
+  fastify.log.info(`server listening on ${address}`);
+});
